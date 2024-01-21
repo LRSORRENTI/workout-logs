0f79f34750e01cd21ab3d9bb4cb8b8879f963085
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-21 09:16:56 -0600

    reworking the main table sections, removed all differing classes, needs to be uniform

--- src/components/WorkoutTable.tsx
+++ src/components/WorkoutTable.tsx
@@ -9,63 +9,10 @@ import {
   } from "@/components/ui/table"
   export default function WorkoutTable() {
     return (
-        <div className="flex justify-center">
-    <div className="" style={{width: "500px"}}>
-    {/* <div className="flex flex-col sm:flex-row justify-evenly "> */}
-    <div className="grid grid-flow-dense ">
-  <div className="">
+        <div className="flex flex-wrap bg-orange-400">
   <Table>
   <TableHeader>
-    <div className="flex justify-center ">
   <h2 className="" style={{width: "300px"}}>Barbell Curls</h2>
-  </div>
-    <TableRow>
-    {/* <h2>barbell curl</h2> */}
-      <TableHead className="w-[100px]">Set</TableHead>
-      <TableHead>Reps</TableHead>
-      <TableHead>Weight</TableHead>
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    <TableRow>
-      <TableCell className="font-medium ml">1</TableCell>
-      <TableCell>10</TableCell>
-      <TableCell>40lbs</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell className="font-medium">2</TableCell>
-      <TableCell >8</TableCell>
-      <TableCell>50lbs</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell className="font-medium">3</TableCell>
-      <TableCell>6</TableCell>
-      <TableCell>60lbs</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell className="font-medium">4</TableCell>
-      <TableCell>5</TableCell>
-      <TableCell>65lbs</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell className="font-medium">5</TableCell>
-      <TableCell>4</TableCell>
-      <TableCell>70lbs</TableCell>
-    </TableRow>
-    <TableRow>
-      <TableCell className="font-medium">6</TableCell>
-      <TableCell>3</TableCell>
-      <TableCell>70lbs</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
-</div>
-<div>
-<Table>
-  <TableHeader>
-  <div className="flex justify-center">
-  <h2 className="" style={{ width: "300px"}}>Lat Pulldowns</h2>
-  </div>
     <TableRow>
       <TableHead className="w-[100px]">Set</TableHead>
       <TableHead>Reps</TableHead>
@@ -107,9 +54,7 @@ import {
 </Table>
 <Table>
   <TableHeader>
-  <div className="flex justify-center">
-  <h2 className="" style={{ minWidth: "300px"}}>Barbell Rows</h2>
-  </div>
+  <h2 className="" style={{width: "300px"}}>Lat Pulldowns</h2>
     <TableRow>
       <TableHead className="w-[100px]">Set</TableHead>
       <TableHead>Reps</TableHead>
@@ -151,9 +96,8 @@ import {
 </Table>
 <Table>
   <TableHeader>
-  <div className="flex justify-center">
-  <h2 className="" style={{ minWidth: "300px"}}>Concentration Curls</h2>
-  </div>
+  <h2 className="" style={{width: "300px"}}>Barbell Rows</h2>
+ 
     <TableRow>
       <TableHead className="w-[100px]">Set</TableHead>
       <TableHead>Reps</TableHead>
@@ -195,9 +139,49 @@ import {
 </Table>
 <Table>
   <TableHeader>
-  <div className="flex justify-center">
+  <h2 className="" style={{ width: "300px"}}>Concentration Curls</h2>
+    <TableRow>
+      <TableHead className="w-[100px]">Set</TableHead>
+      <TableHead>Reps</TableHead>
+      <TableHead>Weight</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium ml">1</TableCell>
+      <TableCell>10</TableCell>
+      <TableCell>40lbs</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">2</TableCell>
+      <TableCell >8</TableCell>
+      <TableCell>50lbs</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">3</TableCell>
+      <TableCell>6</TableCell>
+      <TableCell>60lbs</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">4</TableCell>
+      <TableCell>5</TableCell>
+      <TableCell>65lbs</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">5</TableCell>
+      <TableCell>4</TableCell>
+      <TableCell>70lbs</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell className="font-medium">6</TableCell>
+      <TableCell>3</TableCell>
+      <TableCell>70lbs</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+<Table>
+  <TableHeader>
   <h2 className="" style={{ width: "300px"}}>Hammer Curls</h2>
-  </div>
     <TableRow>
       <TableHead className="w-[100px]">Set</TableHead>
       <TableHead>Reps</TableHead>
@@ -239,9 +223,7 @@ import {
 </Table>
 <Table>
   <TableHeader>
-  <div className="flex justify-center">
   <h2 className="" style={{ width: "300px"}}>Close Grip Barbell Curls</h2>
-  </div>
     <TableRow>
       <TableHead className="w-[100px]">Set</TableHead>
       <TableHead>Reps</TableHead>
@@ -281,9 +263,6 @@ import {
     </TableRow>
   </TableBody>
 </Table>
-</div>
-</div>
-</div>
 </div>
     );
   };
