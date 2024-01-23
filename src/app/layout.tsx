89bd71a62630a00294aa244ca432b9584da40277
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-23 09:18:21 -0600

    added rem padding to main h1, simulating tailwind rem units

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
         <nav className='flex justify-center'>
-          <h1>
+          <h1 className='main-h1'>
             Workout Logs
           </h1>
           {/* <ModeToggle/> */}
